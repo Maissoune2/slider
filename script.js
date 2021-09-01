e40f7d8c4f51f30e08e6fd556717b8d3273408cf
OUABDESSELAM-maissoune <maissounhala35@gmail.com>
2021-09-01 19:13:19 +0100

    simple image slider

---
+++ script.js
@@ -0,0 +1,32 @@
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+    var indexValue = 1 ;
+
+    showImg(indexValue);
+
+    function btm_slide(e){showImg(indexValue = e);}
+
+    function side_slide(e){showImg(indexValue += e);}
+
+        function showImg(e){
+            var i;
+
+            const img = document.querySelectorAll('img');
+
+            const sliders = document.querySelectorAll('.btm_sliders span');
+
+            if (e > img.length) {indexValue = 1;}
+
+            if (e < 1) {indexValue = img.length;}
+
+            for(i = 0; i < img.length; ++i){
+                img[i].style.display = "none";
+            }
+
+            for(i = 0; i < sliders.length; ++i){
+                sliders[i].style.background = "rgba(255,255,255,0.6)";
+            }
+
+            img[indexValue-1].style.display = "block";
+            sliders[indexValue-1].style.background = "#000";
+        }
